461f970b31ea2f0c2506368939d0f743ed339f36
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2021-07-29 19:51:58 -0500

    feat(types): add weather api types

---
+++ src/types/weather-api.d.ts
@@ -0,0 +1,62 @@
+interface Coord {
+  lon: number;
+  lat: number;
+}
+
+export type WeatherCondition =
+  | 'Thunderstorm'
+  | 'Drizzle'
+  | 'Rain'
+  | 'Snow'
+  | 'Atmosphere'
+  | 'Clear'
+  | 'Clouds';
+
+interface Weather {
+  id: number;
+  main: WeatherCondition;
+  description: string;
+  icon: string;
+}
+
+export interface Main {
+  temp: number;
+  pressure: number;
+  humidity: number;
+  temp_min: number;
+  temp_max: number;
+}
+
+interface Wind {
+  speed: number;
+  deg: number;
+  gust: number;
+}
+
+interface Clouds {
+  all: number;
+}
+
+interface Sys {
+  type: number;
+  id: number;
+  message: number;
+  country: string;
+  sunrise: number;
+  sunset: number;
+}
+
+export interface CurrentWeatherDataResponse {
+  coord: Coord;
+  weather: Weather[];
+  base: string;
+  main: Main;
+  visibility: number;
+  wind: Wind;
+  clouds: Clouds;
+  dt: number;
+  sys: Sys;
+  id: number;
+  name: string;
+  cod: number;
+}
