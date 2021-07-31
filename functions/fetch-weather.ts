84d20de78518f8d774015b1b07c5531cafd81e33
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2021-07-30 21:14:48 -0500

    feat(netlify): add query parameters to the fetch-weather function

--- functions/fetch-weather.ts
+++ functions/fetch-weather.ts
@@ -2,20 +2,17 @@ import { CurrentWeatherDataResponse } from './../src/types/weather-api.d';
 import { Handler } from '@netlify/functions';
 import axios from 'axios';
 
-const handler: Handler = async () => {
-  let latitude: number, longitude: number;
-  navigator.geolocation.getCurrentPosition((position) => {
-    latitude = position.coords.latitude;
-    longitude = position.coords.longitude;
-  });
+const handler: Handler = async (event) => {
+  const { lat, lon } = event.queryStringParameters;
+
   const { API_KEY } = process.env;
 
   const response = await axios.get<CurrentWeatherDataResponse>(
     'http://api.openweathermap.org/data/2.5/weather',
     {
       params: {
-        lat: latitude,
-        lon: longitude,
+        lat: lat,
+        lon: lon,
         appid: API_KEY,
         units: 'metric',
       },
