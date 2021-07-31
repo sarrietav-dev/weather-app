766826fc3a8c55cd37650eaa80f4237879752b2b
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2021-07-30 21:09:26 -0500

    feat(netlify): add netlify function
    This was made to hide the API

---
+++ functions/fetch-weather.ts
@@ -0,0 +1,31 @@
+import { CurrentWeatherDataResponse } from './../src/types/weather-api.d';
+import { Handler } from '@netlify/functions';
+import axios from 'axios';
+
+const handler: Handler = async () => {
+  let latitude: number, longitude: number;
+  navigator.geolocation.getCurrentPosition((position) => {
+    latitude = position.coords.latitude;
+    longitude = position.coords.longitude;
+  });
+  const { API_KEY } = process.env;
+
+  const response = await axios.get<CurrentWeatherDataResponse>(
+    'http://api.openweathermap.org/data/2.5/weather',
+    {
+      params: {
+        lat: latitude,
+        lon: longitude,
+        appid: API_KEY,
+        units: 'metric',
+      },
+    },
+  );
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(response.data),
+  };
+};
+
+export { handler };
